eb6cb438e555ede2d4e27e6bbb7ac7189d8abccc
Edasgh <edas25564@gmail.com>
2023-07-13 13:37:30 +0530

    "Added comments"

--- js-async-projects/project_1/index.js
+++ js-async-projects/project_1/index.js
@@ -1,6 +1,8 @@
+// getting the start and stop button elements
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 
+//the function to generate a random color
 const generateRandomCol = () => {
   let hash = "#";
   let num = Math.floor(Math.random() * 16777215);
@@ -9,8 +11,10 @@ const generateRandomCol = () => {
   return color;
 };
 
+// initiate the intervalId
 let intervalId
 
+// function to start changing color
 function startChangingColor() {
   function changeBgCol() {
     document.body.style.backgroundColor = generateRandomCol();
@@ -21,9 +25,13 @@ function startChangingColor() {
   
 }
 
+// function to stop changing color
 function stopChangingColor() {
   clearInterval(intervalId);
 }
+
+
+// referrencing the functions to the button click events
 
 startBtn.addEventListener("click", startChangingColor);
 
