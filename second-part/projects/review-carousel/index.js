62ebca6d8f32b3a794d54fe0e9581c5c720d4919
Edasgh <edas25564@gmail.com>
2023-07-25 19:20:27 +0530

    "carousel work started"

---
+++ second-part/projects/review-carousel/index.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll(".review-card");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let index = 0;
+// let translateX = -461;
+// let anotherIndx=4
+
+
+
+
