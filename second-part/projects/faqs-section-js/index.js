2f5f141282017f73dc909184819cadbfeef45fa6
Edasgh <edas25564@gmail.com>
2023-07-26 12:17:37 +0530

    "FAQs-section complete"

--- second-part/projects/faqs-section-js/index.js
+++ second-part/projects/faqs-section-js/index.js
@@ -1,5 +1,17 @@
-const qaDivs=document.querySelectorAll("main>.qa");
+//getting all the question-answer divs
+const qaDivs = document.querySelectorAll(".qa");
 
-qaDivs.forEach((div)=>{
-    
+//for each question if user clicks on plus button , show the answer
+qaDivs.forEach((div) => {
+  const headingBorder = div.querySelector("hr");
+  const answerDiv = div.querySelector(".ans");
+
+  const seeAnsBtn = div.querySelector(".plus-icon");
+
+  seeAnsBtn.onclick = function () {
+    this.classList.toggle("fa-square-minus");
+    this.classList.toggle("fa-square-plus");
+    answerDiv.classList.toggle("hide");
+    headingBorder.classList.toggle("hide");
+  }
 })
