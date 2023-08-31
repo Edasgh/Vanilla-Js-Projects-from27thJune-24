a27ca8e24928cd17755cf1dc0026a974c63c00b1
Edasgh <edas25564@gmail.com>
2023-08-31 20:05:57 +0530

    "Detailed comments added & design changed"

--- second-part/projects/diff-tabs-js/script.js
+++ second-part/projects/diff-tabs-js/script.js
@@ -1,16 +1,20 @@
-const home_menu=document.querySelector(".home-tab-menu");
-const about_menu=document.querySelector(".about-tab-menu");
-const contact_menu=document.querySelector(".contact-tab-menu");
+//getting the menus in the navbar
+const home_menu=document.querySelector(".home-tab-menu"),
+about_menu=document.querySelector(".about-tab-menu"),
+ contact_menu=document.querySelector(".contact-tab-menu");
 
+// getting all the tabs
+const home_tab=document.querySelector(".home-tab"),
+ about_tab=document.querySelector(".about-tab"),
+ contact_tab=document.querySelector(".contact-tab");
 
-const home_tab=document.querySelector(".home-tab");
-const about_tab=document.querySelector(".about-tab");
-const contact_tab=document.querySelector(".contact-tab");
-
+ //on loading the window, only show and select the home-tab
 window.onload=function(){
     about_tab.style.display="none"
     contact_tab.style.display="none"
 }
+
+// after clicking on a menu, show its coresponding tab and hide the another two tabs
 
 home_menu.onclick=function(){
     home_tab.style.display=""
