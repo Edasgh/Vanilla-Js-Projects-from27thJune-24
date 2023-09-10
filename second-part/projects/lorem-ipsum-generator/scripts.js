301c5e4e1a88fc534960388a583666d8f89fb081
Edasgh <edas25564@gmail.com>
2023-09-10 14:41:39 +0530

    "completed, not responsive yet"

---
+++ second-part/projects/lorem-ipsum-generator/scripts.js
@@ -0,0 +1,75 @@
+//given paragraphs array
+const paragraphs = [
+  `Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.`,
+  `Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.`,
+  `Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.`,
+  `Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers kitty power ignore the squirrels, you'll never catch them anyway for what a cat-ass-trophy! or purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Pretend you want to go out but then don't bite off human's toes, yet disappear for four days and return home with an expensive injury; bite the vet so catch eat throw up catch eat throw up bad birds. `,
+  `This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them.
+  I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Michelle, I don't regret this, but I both rue and lament it. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat.`,
+  `Airedale hard cheese mozzarella. Pecorino melted cheese port-salut emmental babybel cheese and wine melted cheese manchego. Everyone loves blue castello everyone loves fromage cheese slices airedale cheddar cream cheese. Bavarian bergkase who moved my cheese halloumi port-salut gouda jarlsberg ricotta rubber cheese. Stinking bishop smelly cheese brie.`,
+  `Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably haven't heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.`,
+  `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
+  `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
+];
+
+//getting the form-field, the no. of paragraps input and the paragraphs container
+
+const formField = document.getElementById("form-field");
+const paragraphNoInput = document.getElementById("para-no");
+
+const paragraphsContainer = document.querySelector(".paragraphs-container");
+
+//setting the default no of pages to be generated
+let defaultNo = 5;
+paragraphNoInput.value = defaultNo;
+
+// shuffling the paragraph array to generate paragraphs in a random order
+const shuffle = (array) => {
+  // create a copy of the array so that the original array is not mutated
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
+
+// function to generate the paragraphs
+const generateParagraphs = (value) => {
+  //converting the no of pages string value to number
+  const num = parseFloat(value);
+  //if user writes any number like 0, or less than 0, don't generate paragraphs
+  if (num <= 0) {
+    alert("An Unknown Error Ocurred!");
+  } else {
+    //if the given input is greater than the length of the array
+    if (num > paragraphs.length) {
+      // shuffle the array
+      let arr = shuffle(paragraphs);
+      let generatedResult = arr.slice(0, num);
+      //calculating the difference between the given input and the length of the array
+      let diff = num - paragraphs.length;
+      for (let i = 0; i < diff; i++) {
+        //then generate other paragraphs randomly from the array
+        let randomIndx = Math.floor(Math.random() * diff);
+        generatedResult.push(paragraphs[randomIndx]);
+      }
+      //show all the elements in document
+      paragraphsContainer.innerHTML = generatedResult.join("<br/><br/><br/>");
+    } else {
+      // if the input isn't greater than the length of the array
+
+      //shuffle the array
+      let arr = shuffle(paragraphs);
+      let generatedResult = arr.slice(0, num);
+      //show all the elements in document
+      paragraphsContainer.innerHTML = generatedResult.join("<br/><br/><br/>");
+    }
+  }
+};
+
+//on submitting the form don't reload page and generate paragraphs as per user input
+formField.addEventListener("submit", (e) => {
+  e.preventDefault();
+  generateParagraphs(paragraphNoInput.value);
+});
